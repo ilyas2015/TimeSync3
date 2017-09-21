034721481e34648b57369e4a1f6fd9ef8ab2d7dd
Ilyas Nasirov <i.nasirov@hotmail.com>
2017-09-20 21:38:25 -0400

    Add project files.

---
+++ TimeSync3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimeSync3.MvcApplication" Language="C#" %>
---
+++ TimeSync3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeSync3.Startup))]
+namespace TimeSync3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
